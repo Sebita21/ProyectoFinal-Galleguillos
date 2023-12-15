628f90bfb546f95f61647f5089cf9461b6025e93
Sebastián Galleguillos Avendaño <sebastiiaang@gmail.com>
2023-12-14 22:50:09 -0300

    ACTUALIZACION DE PROYECTO

--- JS/carrito.js
+++ JS/carrito.js
@@ -1,112 +1,161 @@
-
-const file = 
+const file =
   'https://raw.githubusercontent.com/Sebita21/Entregable2-Galleguillos/main/DATA/productos.json';
 
 let itemsCarrito = [];
 let totalCarrito = 0;
 
 async function cargarProducto() {
+  const producto = await realizarPeticion(file);
 
-  const  producto = await realizarPeticion(file);
+  const productoList = document.getElementById('producto-list');
 
-   const productoList = document.getElementById("producto-list");
+  producto.forEach((producto) => {
+    const productoDiv = document.createElement('div');
+    productoDiv.classList.add('producto');
 
-   producto.forEach((producto) => {
-     const productoDiv = document.createElement("div");
-     productoDiv.classList.add("producto");
-
-     productoDiv.innerHTML = `
+    productoDiv.innerHTML = `
       <img src="${producto.imgen}" alt="${producto.marca}">
       <h3>${producto.marca}</h3>
       <p>Modelo: ${producto.modelo}</p>
       <p>Precio: $${producto.precio}</p>
       <button class="agregar-en-carrito" onclick="agregarCarrito(${producto.id}, '${producto.marca}', ${producto.precio})">Agregar al carrito</button>
     `;
+    productoList.appendChild(productoDiv);
+  });
+}
 
-     productoList.appendChild(productoDiv);
-   });
- }
- window.onload = function () {
-   const almacenarCarrito = JSON.parse(localStorage.getItem("itemsCarrito"));
-   const almacenarTotal = localStorage.getItem("totalCarrito");
+window.onload = function () {
+  const almacenarCarrito = JSON.parse(localStorage.getItem('itemsCarrito'));
+  const almacenarTotal = localStorage.getItem('totalCarrito');
 
-   if (almacenarCarrito && almacenarTotal) {
-     itemsCarrito = almacenarCarrito;
-     totalCarrito = parseFloat(almacenarTotal);
-     guardarCarrito();
-
-   }
-
-   cargarProducto();
- };
-
- function agregarCarrito(productoId, productoMarca, precio) {
-   itemsCarrito.push({ id: productoId, marca: productoMarca, precio: precio });
-   totalCarrito += precio;
-
-   localStorage.setItem("itemsCarrito", JSON.stringify(itemsCarrito));
-   localStorage.setItem("totalCarrito", totalCarrito.toString());
-
-   guardarCarrito();
- }
-
- function eliminarProducto(elimiId, elimPrecio){
-    itemsCarrito = itemsCarrito.filter(item=> item.id !== elimiId);
-    totalCarrito -= elimPrecio;
-
-    localStorage.setItem ('itemsCarrito', JSON.stringify(itemsCarrito));
-    localStorage.setItem('totalCarrito', totalCarrito.toString()) ;
-
+  if (almacenarCarrito && almacenarTotal) {
+    itemsCarrito = almacenarCarrito;
+    totalCarrito = parseFloat(almacenarTotal);
     guardarCarrito();
- }
+  }
 
- function guardarCarrito() {
+  cargarProducto();
+};
 
+function agregarCarrito(productoId, productoMarca, precio) {
+  itemsCarrito.push({ id: productoId, marca: productoMarca, precio: precio });
+  totalCarrito += precio;
+
+  localStorage.setItem('itemsCarrito', JSON.stringify(itemsCarrito));
+  localStorage.setItem('totalCarrito', totalCarrito.toString());
+
+  guardarCarrito();
+}
+function eliminarProducto(elimiId, elimPrecio) {
+  Swal.fire({
+    title: '¿Estás seguro?',
+    text: '¡No podrás revertir esto!',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'eliminar',
+    cancelButtonText: 'Cancelar',
+  }).then((result) => {
+    if (result.isConfirmed) {
+      itemsCarrito = itemsCarrito.filter((item) => item.id !== elimiId);
+      totalCarrito -= elimPrecio;
+
+      localStorage.setItem('itemsCarrito', JSON.stringify(itemsCarrito));
+      localStorage.setItem('totalCarrito', totalCarrito.toString());
+
+      guardarCarrito();
+    }
+  });
+}
+
+function guardarCarrito() {
   const carritoList = document.getElementById('list-carrito');
   carritoList.innerHTML = '';
 
-  itemsCarrito.forEach(item => {
+  itemsCarrito.forEach((item) => {
     const li = document.createElement('li');
     li.innerHTML = `
-      <span>${item.marca} - $${item.precio}</span>
-      <button class="fa fa-trash" onclick="eliminarProducto(${item.id}, ${item.precio})"></button>
-      
+    <span>${item.marca} - $${item.precio}</span>
+    <button class="fa fa-trash" onclick="eliminarProducto(${item.id}, ${item.precio})">
+    </button>
     `;
+
     carritoList.appendChild(li);
   });
 
-  document.getElementById("total-carrito").textContent = totalCarrito;
+  const eliminarBotones = document.querySelectorAll('.fa-trash');
+  eliminarBotones.forEach((boton) => {
+    boton.addEventListener('click', (evento) => {
+      const id = evento.target.dataset.item.id;
+      const precio = evento.target.dataset.item.precio;
+      eliminarProducto(id, precio);
+    });
+  });
+
+  document.getElementById('total-carrito').textContent = totalCarrito;
 }
- 
- function totalApagar() {
-   if (itemsCarrito.length > 0) {
-      Swal.fire(`Total a Pagar: $${totalCarrito}`);
-     itemsCarrito = [];
-     totalCarrito = 0;
 
-     localStorage.removeItem("itemsCarrito");
-     localStorage.removeItem("totalCarrito");
+function totalApagar() {
+  if (itemsCarrito.length > 0) {
+    Swal.fire({
+      title: '¿Estás seguro de pagar?',
+      text: '¡No podrás revertir esto!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Pagar Producto',
+      cancelButtonText: 'Cancelar',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const compraRealizadaEvent = new CustomEvent('compraRealizada', {
+          detail: {
+            totalCarrito: totalCarrito,
+          },
+        });
+        document.dispatchEvent(compraRealizadaEvent);
 
-     guardarCarrito();
-   } else {
-   
-    Swal.fire(
-      'El carrito está vacío. ¡Agrega productos antes de realizar el pedido!'
-    );
-   }
- }
+        itemsCarrito = [];
+        totalCarrito = 0;
 
- async function realizarPeticion(datos){
-    try{
-      const response = await fetch(datos);
+        localStorage.removeItem('itemsCarrito');
+        localStorage.removeItem('totalCarrito');
 
-      if (!response.ok) {
-        throw new Error(`Error en la Petición: ${response.status} ${response.statusText}`);
+        guardarCarrito();
       }
-       const data = await response.json();
+    });
+  } else {
+    Swal.fire({
+      icon: 'error',
+      title: 'Lo Sentimos',
+      text: 'El carrito no se debe encontrar vacido!!',
+    });
+  }
+}
 
-       return data;  
-    }catch(error){
-        console.error(error)
+document.addEventListener('compraRealizada', (event) => {
+  const { totalCarrito } = event.detail;
+  Swal.fire(
+    '¡Tu Compra fue Realizada!',
+    `Total a Pagar: $${totalCarrito}`,
+    'success'
+  );
+});
+
+async function realizarPeticion(datos) {
+  try {
+    const response = await fetch(datos);
+
+    if (!response.ok) {
+      throw new Error(
+        `Error en la Petición: ${response.status} ${response.statusText}`
+      );
     }
- }
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
--- JS/login.js
+++ JS/login.js
@@ -5,65 +5,72 @@ class App {
   };
 
   static initializeData() {
-    const nombre = document.getElementById("nombre");
-    const email = document.getElementById("email");
-    const pass = document.getElementById("password");
-    const form = document.getElementById("form-registro");
-    const parrafo = document.getElementById("warning");
+    const nombre = document.getElementById('nombre');
+    const email = document.getElementById('email');
+    const pass = document.getElementById('password');
+    const form = document.getElementById('form-registro');
+    const parrafo = document.getElementById('warning');
 
-    form.addEventListener("submit", (e) => {
+    form.addEventListener('submit', (e) => {
       e.preventDefault();
-      let warning = "";
+      let warning = '';
       let entrar = false;
       let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
-      parrafo.innerHTML = "";
+      parrafo.innerHTML = '';
 
       if (nombre.value.length < 6) {
-        warning += "El nombre es muy corto <br>";
+        warning += 'El nombre es muy corto <br>';
         entrar = true;
       }
 
       if (!regexEmail.test(email.value)) {
-        warning += "El email no es valido <br>";
+        warning += 'El email no es valido <br>';
         entrar = true;
       }
 
       if (pass.value.length < 8) {
-        warning += "La contraseña no es valida<br>";
+        warning += 'La contraseña no es valida<br>';
         entrar = true;
       }
-    
+
       if (entrar) {
         parrafo.innerHTML = warning;
       } else {
-        parrafo.innerHTML = "Enviado";
-        alert("tu registro fue guardado en el sessionStorage");
+        parrafo.innerHTML = 'Enviado';
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: 'Tu registro fue guardado en el SessionStorage',
+          showConfirmButton: false,
+          timer: 1500,
+        });
         const datoUsuario = {
           nombre: nombre.value,
           email: email.value,
           pass: pass.value,
         };
-         sessionStorage.setItem("datoUsuario", JSON.stringify(datoUsuario));
+
+        sessionStorage.setItem('datoUsuario', JSON.stringify(datoUsuario));
+   
       }
     });
-
-    let signUp = document.getElementById("signUp");
-    let sigIn = document.getElementById("signIn");
-    let nomInput = document.getElementById("nomInput");
-    let titulo = document.getElementById("titulo");
+    let signUp = document.getElementById('signUp');
+    let sigIn = document.getElementById('signIn');
+    let nomInput = document.getElementById('nomInput');
+    let titulo = document.getElementById('titulo');
 
     signIn.onclick = function () {
-      nomInput.style.maxHeight = "0";
-      titulo.innerHTML = "Login";
-      signUp.classList.add("disable");
-      sigIn.classList.remove("disable");
+      nomInput.style.maxHeight = '0';
+      titulo.innerHTML = 'Login';
+      signUp.classList.add('disable');
+      sigIn.classList.remove('disable');
     };
 
     signUp.onclick = function () {
-      nomInput.style.maxHeight = "60px";
-      titulo.innerHTML = "Registro";
-      signUp.classList.remove("disable");
-      sigIn.classList.add("disable");
+      nomInput.style.maxHeight = '60px';
+      titulo.innerHTML = 'Registro';
+      signUp.classList.remove('disable');
+      sigIn.classList.add('disable');
     };
   }
 
@@ -73,4 +80,3 @@ class App {
 }
 
 App.start();
-
